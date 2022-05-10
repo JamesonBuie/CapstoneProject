e1313964b0f0711b98f153e277c7bad564892a86
Jameson Buie <jamesonbuie@gmail.com>
2022-05-10 12:54:22 -0500

    Began creation of multiple objects

---
+++ script.js
@@ -0,0 +1,58 @@
+// Basic object template
+const objectName = {
+    ribLickers: ``,
+    bigMan: ``,
+    yanceys: ``,
+    galeNDales: ``,
+    fineArtsBistro: ``,
+    georgeJs: ``,
+    familyStyle: ``,
+    mjkFeedMeal: ``,
+    ritterHouse: ``,
+    kanhaKafe: ``,
+    ajs: ``,
+    bbqHut: ``,
+    cowboys: ``,
+    bullys: ``,
+    newCentury: ``,
+    smokinEats: ``,
+    bestDonuts: ``,
+    sweetheartBakery: ``
+}
+
+
+// Website links for all the restaurants
+const websiteLinks = {
+    ribLickers: `https://riblickers.com/`,
+    bigMan: `https://big-man-bbq.hub.biz/`,
+    yanceys: `https://yanceysbrewery.com/`,
+    galeNDales: `https://www.menupix.com/kentucky/restaurants/1701647/Gale-n-Dales-Glasgow-KY`,
+    fineArtsBistro: `https://www.facebook.com/fineartsbistro/`,
+    georgeJs: `https://www.facebook.com/George-Js-On-The-Square-100867173290749/`,
+    familyStyle: `http://glasgowfamilystylerestaurant.com/home/menu/`,
+    mjkFeedMeal: `https://www.facebook.com/FEEDMEAL/`,
+    ritterHouse: `http://ritterhouserestaurant.com/`,
+    kanhaKafe: `https://www.facebook.com/KanhaKafeGlasgow/`,
+    ajs: `https://www.facebook.com/AJS-Restaurant-INC-327771524581522/`,
+    bbqHut: `https://thebarbqhut.com/?fbclid=IwAR3ZSn4VA6_5rYUO7rX7J2Co3xuAh9W4M3NaToXZGdiX8-N48yYKg1CLARY`,
+    cowboys: `https://www.facebook.com/Cowboys-Country-Cookin-1827114997584354/`,
+    bullys: `https://bullys-on-main-st.business.site/?fbclid=IwAR3MixKACKgk2FobcHvQgCQLIb72ETkyiGXpOAPhf1RV4mmUC9PDEZJsOvs`,
+    newCentury: `https://www.newcenturyshelbyville.com/`,
+    smokinEats: `https://www.facebook.com/LetsGetCooking1/`,
+    bestDonuts: `https://www.facebook.com/profile.php?id=100057331486704`,
+    sweetheartBakery: `https://www.facebook.com/sweetheartbakeryglasgow/`
+}
+
+// Names of restaurants
+const titles = {
+  
+}
+
+
+
+
+
+// Function to display restaurant website link on page
+
+
+
